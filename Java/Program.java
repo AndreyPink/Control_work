fa73335b89612c034034f1489180c5ce26c2b3df
ANDREY PINK <andreypink@bk.ru>
2023-05-25 15:58:38 +0300

    update

--- Java/Program.java
+++ Java/Program.java
@@ -1,10 +1,13 @@
 package Java;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.PriorityQueue;
 
 public class Program {
 
     public static void main(String[] args) {
+
         Toy toy1 = new Toy(1, 2, "конструктор");
         Toy toy2 = new Toy(2, 2, "робот");
         Toy toy3 = new Toy(3, 6, "кукла");
@@ -15,10 +18,19 @@ public class Program {
         queue.add(toy2);
         queue.add(toy3);
 
-        for (int index = 0; index < 10; index++) {
-            System.out.printf("id: %d, weight: %d, name: %s", queue.peek().getId(), queue.peek().getWeight(),
-                    queue.peek().getName());
-            System.out.println();
+
+        try (FileWriter writer = new FileWriter("Java/randomToy.txt")) {
+            for (int index = 0; index < 10; index++) {
+                PriorityQueue<Toy> randomQueue = queue;
+                String text = String.format("id: %d, weight: %d, name: %s", randomQueue.peek().getId(),
+                randomQueue.peek().getWeight(),
+                randomQueue.peek().getName());
+                writer.write(text);
+                writer.append('\n');
+            }
+        } catch (IOException ex) {
+
+            System.out.println(ex.getMessage());
         }
     }
 }
